c6bf2bdaa9cfbeb25705734747926eab7ae62ab8
John Sawyer <jsawyer@stetson.edu>
2018-02-21 13:47:13 -0500

    finished "double"

--- ParseArgv.php
+++ ParseArgv.php
@@ -3,13 +3,17 @@ class ParseArgv
 {
 private $argsParsed;
 private $argsUnparsed;
-
+private $DoubleEXP;
+private $DoubleName;
+private $DoubleNameEXP;
     /**
      * ParseArgv constructor.
      * @param $args
      */
     public function __construct($args)
 {
+    $this->DoubleEXP = array();
+    $this->DoubleNameEXP = array();
 $this->argsUnparsed = $args;
 $this->argsParsed = array(
 
@@ -19,6 +23,9 @@ $this->argsParsed = array(
 }
 
 
+    /**
+     * @return array
+     */
     public function getParsed()
 {
     $ImpParsed =array();
@@ -26,23 +33,44 @@ $this->argsParsed = array(
     {
         $ImpParsed = explode(' ', $k);
         print_r($ImpParsed);
+
     }
 
 
     foreach ($ImpParsed as $k)
     {
-
+        $string=current($ImpParsed);
+       next($ImpParsed);
             if(substr("$k",0,2)== '--') {
-                $this->argsUnparsed['double'][]= $k;
-
+                $this->DoubleEXP = explode('=',$k);
+                foreach($this->DoubleEXP as $items)
+                {
+                    if(substr("$items",0,2)== '--')
+                    {
+                       $this->DoubleName=substr("$items",2);
+                    }
+                    else
+                    {
+                        $this->DoubleNameEXP = explode(',',$items);
+                        print_r($this->DoubleNameEXP);
+                        $Counter = 0;
+                        foreach($this->DoubleNameEXP as $value)
+                        {
+                            $this->argsUnparsed['double'][$this->DoubleName] .=" [$Counter]  '$value'  ,  ";
+                            $Counter++;
+                        }
+                        $this->argsUnparsed['double'][$this->DoubleName] .= " ($Counter arguments) ";
+                    }
+                }
             }
             elseif(substr("$k",0,1)== '-')
             {
-                if(substr("$k",0,1)!= '-')
+                if(substr("$string",0,1)!= '-')
                 {
-
+                    $this->argsUnparsed['Single'][$k]=$string;
                 }
                 else {
+
                    $this->argsUnparsed['flag'][] = $k;
                 }
             }
@@ -50,9 +78,15 @@ $this->argsParsed = array(
 
         foreach($this->argsUnparsed as $k=>$v)
         {
+            echo "<br>";
+            print("$k");
+            echo"<br>";
             foreach($v as $t=>$r)
             {
+                echo "<br>";
+                print ("$t ");
                 print("$r  ");
+
             }
         }
 
